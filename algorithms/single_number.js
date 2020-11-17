e7d9a6bcf2223d4e72ebcde8f269283d46800014
Michael Ilchuk <iltchukmihail@gmail.com>
2020-11-17 23:07:37 +0200

    add single_number.js

---
+++ algorithms/single_number.js
@@ -0,0 +1,43 @@
+
+// https://leetcode.com/problems/single-number/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber_f = function(nums) {
+  let uniqueNumber = 0
+  let countMap = new Map()
+
+  nums.forEach(n => { 
+    countMap.set(n, (countMap.get(n) | 0 ) + 1)
+  })
+
+  countMap.forEach((value, key) => {
+    if (value === 1) {
+      uniqueNumber = key
+      return
+    }
+  })
+
+  return uniqueNumber
+};
+
+// Better solution
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let uniqueNumber = 0
+
+  nums.forEach(n => uniqueNumber ^= n )
+
+  return uniqueNumber
+};
+
+
+console.log(singleNumber([2,2,1])) // 1
+console.log(singleNumber([4,1,2,1,2])) // 4
+console.log(singleNumber([1])) // 1
