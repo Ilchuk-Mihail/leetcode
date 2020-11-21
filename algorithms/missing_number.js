475e48b432290633d1b537cc9b304f493804e8c3
Michael Ilchuk <iltchukmihail@gmail.com>
2020-11-21 17:49:16 +0200

    Create missing_number.js

---
+++ algorithms/missing_number.js
@@ -0,0 +1,46 @@
+
+// https://leetcode.com/problems/missing-number/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const missingNumber_f = function(nums) {
+
+  nums = nums.sort((a, b) => a - b)
+
+  if (nums[nums.length - 1] !== nums.length) {
+    return nums.length
+  } else if (nums[0] !== 0) {
+    return 0
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i) {
+      return i
+    }
+  }
+
+  return 0
+}
+
+// Better solution
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const missingNumber = function(nums) {
+  let missing = nums.length
+
+  for (let i = 0; i < nums.length; i++) {
+    missing ^= i ^ nums[i]
+  }
+
+  return missing
+}
+
+console.log(missingNumber([3,0,1])) // 2
+console.log(missingNumber([0,1])) // 2
+console.log(missingNumber([9,6,4,2,3,5,7,0,1])) // 8
